76a074ef6e1848ae8897987a6b96e44e5450f48c
Sachin Kumar <91049993+sachinkumar1469@users.noreply.github.com>
2022-12-22 22:58:49 +0530

    Add files via upload

--- routes/login.js
+++ routes/login.js
@@ -1,7 +1,27 @@
 const express = require('express');
 const router = express.Router();
-const {loginDefault,postLogin,postLogout} = require('../controllers/login')
-router.use('/postlogin',postLogin)
+const {loginDefault,postLogin,postLogout} = require('../controllers/login');
+const {check,body} = require('express-validator/check');
+const Users = require('../models/user')
+router.use('/postlogin',
+    [
+        check('email')
+        .isEmail()
+        .withMessage("Not an valid Email Address")
+        .custom((value,{req})=>{
+            return Users.findOne({email:value})
+            .then(result=>{
+                if(!result){
+                    return Promise.reject("User doesn't exist")
+                } 
+                return Promise.resolve();
+            })
+        }),
+        check("password")
+        .isLength({min:5})
+        .withMessage("Password should be of min length 5")
+    ],
+postLogin)
 router.use('/logout',postLogout)
 router.use('/',loginDefault);
 
--- routes/signup.js
+++ routes/signup.js
@@ -1,9 +1,38 @@
 const express = require('express')
 const router = express.Router();
-const {signUpController,postSignupController} = require('../controllers/signup')
+const {signUpController,postSignupController} = require('../controllers/signup');
+const {check,body} = require('express-validator/check')
+const User = require('../models/user')
 
-
-router.post('/postSignup',postSignupController)
+router.post('/postSignup',
+    [
+        check("email")
+        .isEmail()
+        .withMessage("Please enter valid email address!")
+        .custom((value,{req})=>{
+            return User.findOne({email:value})
+            .then(result=>{
+                if(result){
+                    return Promise.reject("User Already Exists")
+                }
+                return Promise.resolve();
+            })
+        })
+        ,
+        body("password","please enter valid password")
+        .isLength({min:5})
+        .isAlphanumeric()
+        ,
+        body("confirmPassword")
+        .custom((value,{req})=>{
+            if(value !== req.body.password){
+                throw new Error("Password doesn't match");
+            }
+            return true;
+        })
+        
+    ],
+    postSignupController)
 router.get('/',signUpController);
 
 
